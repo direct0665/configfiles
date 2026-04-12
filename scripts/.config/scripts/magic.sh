29f186808844319a566cdb7182e735bce382dc62
Paul Harms <paul.harms@icloud.com>
2026-04-12 02:27:17 +0200

    Update magic.sh

--- scripts/.config/scripts/magic.sh
+++ scripts/.config/scripts/magic.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Auswahlmenü - clean und ohne Symbole wie gewünscht
-op=$(echo -e "power\naudio\nupdate\nclipboard" | wofi -dmenu -p "magic:")
+# 1. Die Liste für Wofi (HIER müssen alle Optionen rein!)
+# Wichtig: Alle Optionen, die du unten im 'case' hast, müssen hier oben stehen.
+options="power\naudio\nupdate\nclipboard\nreadme (local)\ngithub (remote)\ncockpit"
+
+# Auswahlmenü aufrufen
+op=$(echo -e "$options" | wofi -dmenu -p "magic:")
 
 case $op in
     power)
@@ -12,13 +16,11 @@ case $op in
         kitty -e yay -Syu ;;
     clipboard)
         cliphist list | wofi -dmenu | cliphist decode | wl-copy ;;
-    readme (local))
-        # Öffnet die README in kitty. Falls du 'bat' hast, nimm 'bat', sonst 'less'
-        kitty --title "README" -e less "$HOME/configfiles/README.md" ;;
-    github (remote))
-        # Link zu deinem Repo (hier deinen echten Link einsetzen)
+    "readme (local)")
+        # Nutzt jetzt den Symlink aus dem setup.sh
+        kitty --title "README" -e bat --paging=always "$HOME/.config/scripts/current_readme.md" ;;
+    "github (remote)")
         firefox "https://github.com/direct0665/configfiles" ;;
     cockpit)
-        # Direkt zum lokalen Cockpit Dashboard
         firefox "https://localhost:9090" ;;
 esac
