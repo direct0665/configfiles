48f6a29835be6507e803947587b38e920d769799
Paul Harms <paul.harms@icloud.com>
2026-04-12 02:25:29 +0200

    Update magic.sh

--- scripts/.config/scripts/magic.sh
+++ scripts/.config/scripts/magic.sh
@@ -12,10 +12,10 @@ case $op in
         kitty -e yay -Syu ;;
     clipboard)
         cliphist list | wofi -dmenu | cliphist decode | wl-copy ;;
-    "readme (local)")
+    readme (local))
         # Öffnet die README in kitty. Falls du 'bat' hast, nimm 'bat', sonst 'less'
         kitty --title "README" -e less "$HOME/configfiles/README.md" ;;
-    "github (remote)")
+    github (remote))
         # Link zu deinem Repo (hier deinen echten Link einsetzen)
         firefox "https://github.com/direct0665/configfiles" ;;
     cockpit)
